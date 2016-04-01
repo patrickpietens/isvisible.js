d64c3e7b78265fd84e6ba4690c3fb2ff0bc0b77f
Patrick Pietens <hello@patrickpietens.com>
2016-04-01 12:49:10 +0200

    Added readme.md

--- src/isVisible.js
+++ src/isVisible.js
@@ -1,38 +1,36 @@
 'use strict';
 
 /**
- * Checks if a `Node` is visible on screen.
+ * Checks if a `Node` is visible in the viewport.
  * @param  {Node} element 	Node object representing the element which is checked
  * @param  {Number} offset  Number representing the offset which is used while checking. Default is `0`
  */
 function isVisible(element, offset = 0) {
+	// Do nothing when there is no element selected
 	if (!element) {
 		return false;
 	}
 
-	let {
-		top,
-		right,
-		bottom,
-		left,
-		width,
-		height,
-	} = element.getBoundingClientRect();
+	// Get element info
+	const { top, right, bottom, left, width, height, } = element.getBoundingClientRect();
 
-	if (width === 0 ||& height === 0) {
+	// If no width or height is selected return false
+	if (width === 0 || height === 0) {
 		return false;
 	}
 
+	// Get viewport info
 	let myTop = (window.innerHeight || document.documentElement.clientHeight) - offset,
 		myRight = (window.innerWidth || document.documentElement.clientWidth) - offset,
 		myBottom = offset,
 		myLeft = offset;
 
-	let myVisible =
+	// Check if the element is visible in the viewport
+	const isVisible =
 		top <= myTop &&
 		bottom >= myBottom &&
 		right <= myRight &&
 		left >= myLeft;
 
-	return myVisible;
+	return isVisible;
 };
